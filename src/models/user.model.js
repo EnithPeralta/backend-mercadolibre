695da848b275f167ae5b16ffedf8d2d0f974c677
DanielSolarte23 <royersolarte22@gmail.com>
2024-07-29 07:31:42 -0500

    Elimino la cedula del usuario

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,5 @@
 import mongoose from "mongoose"
 const userSchema = mongoose.Schema({
-    cedula:{
-        type:String,
-        required:true,
-        unique:true,
-    },
     nombre:{
         type:String,
         required:true
